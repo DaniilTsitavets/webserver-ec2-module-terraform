862e39720a1d696d22ad045bc1d324cb71643931
Daniil Tsitavets <d.tsitavets@gmail.com>
2025-07-17 13:40:12 +0300

    refactor: shift backend config to main.tf and provide more info about version of tf and aws provider

--- infrastructure/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    encrypt      = true
-    bucket       = "backend-58490149"
-    region       = "eu-north-1"
-    key          = "terraform.tfstate"
-    use_lockfile = true
-  }
-}
